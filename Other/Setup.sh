795f45ebb8c39cb4de6abe5c869ef7267e05111e
Ryan Voit <93566632+Ghost7926@users.noreply.github.com>
2023-07-12 12:06:55 -0400

    Update and rename Setup to Setup.sh

---
+++ Other/Setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "This must be run with root priviledges - gh0st"
+  exit 1
+fi 
+
+cat << "EOF"
+██     ██ ███████ ██      ██████  ██████  ███    ███ ███████ 
+██     ██ ██      ██     ██      ██    ██ ████  ████ ██      
+██  █  ██ █████   ██     ██      ██    ██ ██ ████ ██ █████   
+██ ███ ██ ██      ██     ██      ██    ██ ██  ██  ██ ██      
+ ███ ███  ███████ ███████ ██████  ██████  ██      ██ ███████ 
+                                                             
+  ▄████  ██░ ██  ▒█████    ██████ ▄▄▄█████▓
+ ██▒ ▀█▒▓██░ ██▒▒██▒  ██▒▒██    ▒ ▓  ██▒ ▓▒
+▒██░▄▄▄░▒██▀▀██░▒██░  ██▒░ ▓██▄   ▒ ▓██░ ▒░
+░▓█  ██▓░▓█ ░██ ▒██   ██░  ▒   ██▒░ ▓██▓ ░ 
+░▒▓███▀▒░▓█▒░██▓░ ████▓▒░▒██████▒▒  ▒██▒ ░ 
+ ░▒   ▒  ▒ ░░▒░▒░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░  ▒ ░░   
+  ░   ░  ▒ ░▒░ ░  ░ ▒ ▒░ ░ ░▒  ░ ░    ░    
+░ ░   ░  ░  ░░ ░░ ░ ░ ▒  ░  ░  ░    ░      
+      ░  ░  ░  ░    ░ ░        ░           
+                                           
+EOF
+
+#update repos
+echo "Updating package repos..."
+apt update > /dev/null 2>&1
+echo "Succcess"
+
+# Installing the Tools
+
+applications=("seclists" "bloodhound" "gobuster" "gedit") 
+
+for app in "${applications[@]}"; do
+  echo "Installing $app..."
+  apt install -y "$app" > /dev/null 2>&1
+  echo "Installation of "$app" complete."
+done
+echo "Installation of tools complete.
