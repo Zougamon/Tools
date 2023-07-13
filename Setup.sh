8f63fc240a98cea31cf0a4e2e90a5947a0816feb
Ryan Voit <93566632+Ghost7926@users.noreply.github.com>
2023-07-13 14:43:34 -0400

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -52,13 +52,20 @@ done
 
 # Download sharphound from github
 echo "Downloading SharpHound..."
-for file in $(cat ActiveDirectory/SharpHound/SharpHoundv1.1.1.txt); do 
+mkdir ActiveDirectory/SharpHound
+for file in $(cat ActiveDirectory/URLinks/SharpHoundv1.1.1.txt); do 
   wget -P ActiveDirectory/SharpHound ${file} > /dev/null 2>&1; 
 done
 echo "Download complete."
 echo "Unzipping downloaded folder..."
 unzip ActiveDirectory/SharpHound/SharpHound-v1.1.1.zip -d ActiveDirectory/SharpHound > /dev/null 2>&1
 echo "Unzip complete."
+
+echo "Downloading PowerView..."
+for file in $(cat ActiveDirectory/URLinks/PowerView.txt); do 
+  wget -P ActiveDirectory/PowerView ${file} > /dev/null 2>&1;
+done
+echo "Download complete."
 
 # downloading Impackets
 echo "Downloading impacket..."
@@ -73,25 +80,40 @@ echo "Unzip complete. Further action will be required for impacket."
 
 # Downloading Certipy from github
 echo "Downloading Certipy..."
-for file in $(cat ActiveDirectory/Certipy.txt); do 
+for file in $(cat ActiveDirectory/URLinks/Certipy.txt); do 
   git clone ${file} ActiveDirectory> /dev/null 2>&1; 
 done
 echo "Download complete. Further action will be required to install Certipy."
 
 # Download LdapRelayScan from github
 echo "Downloading LdapRelayScan..."
-for file in $(cat ActiveDirectory/LdapRelayScan.txt); do 
+for file in $(cat ActiveDirectory/URLinks/LdapRelayScan.txt); do 
   git clone ${file} ActiveDirectory> /dev/null 2>&1; 
 done
 echo "Download complete."
 
 # Download PetitPotam
 echo "Downloading PetitPotam..."
-for file in $(cat ActiveDirectory/PetitPotam.txt); do 
+for file in $(cat ActiveDirectory/URLinks/PetitPotam.txt); do 
   git clone ${file} ActiveDirectory> /dev/null 2>&1; 
 done
 echo "Download complete."
 
+# Download Kerbrute
+echo "Downloading Kerbrute..." 
+for file in $(cat ActiveDirectory/URLinks/kerbrute.txt); do 
+  wget -P ActiveDirectory ${file} > /dev/null 2>&1;
+done
+mv kerbrute_linux_amd64 kerbrute
+chmod 744 kerbrute
+echo "Download complete."
+
+# Download PowerSploit
+echo "Downloading PetitPotam..."
+for file in $(cat ActiveDirectory/URLinks/PowerSploit.txt); do 
+  git clone ${file} ActiveDirectory> /dev/null 2>&1; 
+done
+echo "Download complete."
 
 echo ""
 echo "Installation of tools complete. Happy Hacking!"
